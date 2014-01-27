2d7ccc5f032c888b7b4758b28d0714495d61ae76
Balda <xavier@creative-area.net>
2014-01-27 18:03:45 +0100

    tests

---
+++ ImageData.js
@@ -0,0 +1,313 @@
+/*
+* imageData()
+*
+* Depends on
+* https://github.com/mattiasw/ExifReader
+*/
+
+/*
+* - links
+* http://www.html5rocks.com/en/tutorials/canvas/hidpi/ (retina)
+* https://github.com/stomita/ios-imagefile-megapixel (bug ios7 iphone5)
+* https://developer.mozilla.org/en-US/docs/Web/API/File
+* https://github.com/gokercebeci/canvasResize
+* https://github.com/jseidelin/exif-js
+* https://github.com/mattiasw/ExifReader
+*/
+
+;(function(window, $, document, undefined) {
+	var pluginName = "imageData";
+
+	var idFileInput = "file_input_image_data_url";
+
+	var _settings = {
+		url: true, // data URL loaded
+		text: false, // text loaded
+		blob: false, // binary string loaded
+		buffer: false, // array buffer loaded
+		exif: false, // exif data loaded (use https://github.com/mattiasw/ExifReader)
+		resize: false, // resize array
+		complete: $.noop
+	};
+
+	var loadMethod = {
+		"url": "readAsDataURL",
+		"text": "readAsText",
+		"blob": "readAsBinaryString",
+		"buffer": "readAsArrayBuffer"
+	}
+
+	// cache image data
+	var _imageData = {};
+
+	// picker (input file)
+	var picker = false;
+
+
+	/**
+	* Detect subsampling in loaded image.
+	* In iOS, larger images than 2M pixels may be subsampled in rendering.
+	*/
+	function detectSubsampling(img) {
+		var iw = img.naturalWidth, ih = img.naturalHeight;
+		if (iw * ih > 1024 * 1024) { // subsampling may happen over megapixel image
+			var canvas = document.createElement('canvas');
+			canvas.width = canvas.height = 1;
+			var ctx = canvas.getContext('2d');
+			ctx.drawImage(img, -iw + 1, 0);
+			// subsampled image becomes half smaller in rendering size.
+			// check alpha channel value to confirm image is covering edge pixel or not.
+			// if alpha value is 0 image is not covering, hence subsampled.
+			return ctx.getImageData(0, 0, 1, 1).data[3] === 0;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	* Detecting vertical squash in loaded image.
+	* Fixes a bug which squash image vertically while drawing into canvas for some images.
+	*/
+	function detectVerticalSquash(img, iw, ih) {
+		var canvas = document.createElement('canvas');
+		canvas.width = 1;
+		canvas.height = ih;
+		var ctx = canvas.getContext('2d');
+		ctx.drawImage(img, 0, 0);
+		var data = ctx.getImageData(0, 0, 1, ih).data;
+		// search image edge pixel position in case it is squashed vertically.
+		var sy = 0;
+		var ey = ih;
+		var py = ih;
+		while (py > sy) {
+			var alpha = data[(py - 1) * 4 + 3];
+			if (alpha === 0) {
+				ey = py;
+			} else {
+				sy = py;
+			}
+			py = (ey + sy) >> 1;
+		}
+		var ratio = (py / ih);
+		return (ratio===0)?1:ratio;
+	}
+
+	function createPicker() {
+		var $inputFile = $("<input>", {
+			"type": "file",
+			"name": idFileInput,
+			"id": idFileInput,
+			"style": "display:none;"
+		}).on( "change", handlePicker );
+
+		$( "body" ).prepend( $inputFile );
+
+		return $inputFile;
+	}
+
+	function handlePicker(event) {
+		event.preventDefault();
+		event.stopPropagation();
+
+		_imageData = {};
+
+		var files = document.getElementById(idFileInput).files;
+		if (!files.length) {
+			return;
+		}
+		var file = files[0];
+
+		// loading methods
+		var _async = [];
+		for ( var m in loadMethod ) {
+			if ( _settings[ m ] ) {
+				_async.push( methods.load( file, m ) );
+			}
+		}
+		$.when.apply( this, _async ).done(
+			function() {
+				if ( _settings.resize ) {
+					// resize image
+					var _asyncResize = [];
+					$.each(_settings.resize, function(index, val) {
+						_asyncResize.push( methods.resize( _imageData.url, val ) );
+					});
+					$.when.apply( this, _asyncResize ).done(
+						function() {
+							// render image data with resized images
+							_imageData.resize = arguments;
+							_settings.complete( _imageData );
+						}
+					);
+				} else {
+					// render image data
+					_settings.complete( _imageData );
+				}
+			}
+		);
+	}
+
+	var methods = {
+		type: function( dataURL ) {
+			var t = dataURL.split(";");
+			var t2 = t[0].split(":");
+			return t2[1];
+		},
+		resize: function( dataURL, options, callback ) {
+			var defaults = {
+				type: false,
+				width: false,
+				height: false,
+				auto: true
+			}
+			var settings = $.extend( {}, defaults, options ) ;
+			settings.type = settings.type || methods.type( dataURL );
+
+			var image = new Image();
+			image.src = dataURL;
+
+			return $.Deferred(function( defer ) {
+				image.onload = function() {
+					var maxWidth = settings.width || image.width,
+					maxHeight = settings.height || image.height,
+					imageWidth = image.width,
+					imageHeight = image.height;
+
+					if (imageWidth > imageHeight) {
+						if (imageWidth > maxWidth) {
+							imageHeight *= maxWidth / imageWidth;
+							imageWidth = maxWidth;
+						}
+					} else {
+						if (imageHeight > maxHeight) {
+							imageWidth *= maxHeight / imageHeight;
+							imageHeight = maxHeight;
+						}
+					}
+
+					imageWidth = Math.round(imageWidth);
+					imageHeight = Math.round(imageHeight);
+
+					var canvas = document.createElement("canvas");
+					var context = canvas.getContext("2d");
+
+					canvas.width = imageWidth;
+					canvas.height = imageHeight;
+					image.width = imageWidth;
+					image.height = imageHeight;
+					
+					var iw = image.naturalWidth, ih = image.naturalHeight;
+
+					context.save();
+					// transformCoordinate(canvas, width, height, options.orientation);
+					var subsampled = detectSubsampling(image);
+					if (subsampled) {
+						iw /= 2;
+						ih /= 2;
+					}
+					var d = 1024; // size of tiling canvas
+					var tmpCanvas = document.createElement('canvas');
+					tmpCanvas.width = tmpCanvas.height = d;
+					var tmpCtx = tmpCanvas.getContext('2d');
+					// var vertSquashRatio = doSquash ? detectVerticalSquash(image, iw, ih) : 1;
+					var vertSquashRatio = detectVerticalSquash(image, iw, ih);
+					var dw = Math.ceil(d * imageWidth / iw);
+					var dh = Math.ceil(d * imageHeight / ih / vertSquashRatio);
+					var sy = 0;
+					var dy = 0;
+					while (sy < ih) {
+						var sx = 0;
+						var dx = 0;
+						while (sx < iw) {
+							tmpCtx.clearRect(0, 0, d, d);
+							tmpCtx.drawImage(image, -sx, -sy);
+							context.drawImage(tmpCanvas, 0, 0, d, d, dx, dy, dw, dh);
+							sx += d;
+							dx += dw;
+						}
+						sy += d;
+						dy += dh;
+					}
+				    context.restore();
+				    tmpCanvas = tmpCtx = null;
+
+					// Convert the resize image to a new file
+					var newURI = canvas.toDataURL(settings.type);
+
+					var data = {
+						url: newURI, 
+						width: imageWidth,
+						height: imageHeight
+					}
+					defer.resolve( data );
+					if ( callback ) callback( data );
+				};
+				image.onerror = function(error) {
+					defer.reject( error );
+				};
+		    }).promise();
+		},
+		load: function( file, method ) {
+			method = method || "url";
+			method = ( loadMethod[ method ] == undefined ) ? "url" : method;
+			return $.Deferred(function( defer ) {
+				var reader = new FileReader();
+				reader.onloadend = function(evt) {
+					// If we use onloadend, we need to check the readyState.
+					if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+						_imageData[ method ] = evt.target.result;
+						if ( method === "buffer" && _settings.exif ) {
+							try {
+								// exif data
+								// https://github.com/mattiasw/ExifReader
+								var exif = new ExifReader();
+								exif.load( _imageData[ method ] );
+								_imageData.exif = exif.getAllTags();
+							} catch (error) {
+								_imageData.exif = {};
+							}
+						}
+						if ( method === "url" ) {
+							var image = new Image();
+							image.src = _imageData[ method ];
+							image.onload = function() {
+								_imageData.width = image.width,
+								_imageData.height = image.height;
+								defer.resolve(_imageData[ method ]);
+							}
+						} else {
+							defer.resolve(_imageData[ method ]);
+						}
+					}
+				};
+				reader.onabort = function(error) {
+					defer.reject( error );
+				};
+				reader.onerror = function(error) {
+					defer.reject( error );
+				};
+				reader[ loadMethod[ method ] ](file);
+			}).promise();
+		}
+	}
+
+	$.fn[pluginName] = function(options) {
+		var args = arguments;
+
+		if (options === undefined || typeof options === "object") {
+			$.extend( _settings, options || {} ) ;
+			if ( _settings.exif ) _settings.buffer = true;
+			if ( _settings.resize ) _settings.url = true;
+
+			picker = picker || createPicker();
+
+			return this.each(function () {
+				$(this).on( "click", function() {
+					picker.trigger( "click" );
+				});
+			});
+		} else if (typeof options === "string" && methods[options] != undefined) {
+			return methods[options].apply( this, Array.prototype.slice.call( args, 1 ) );
+		}
+	};
+})(window, jQuery, document);
